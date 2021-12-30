119816a5d322f7f9488424490b3d2e0f564abc43
Maxmsl <96868983+Maxmsl@users.noreply.github.com>
2021-12-30 15:51:22 +0100

    Add files via upload

---
+++ Music_Project_JAVA-main/Projet Musique - JAVA/src/interfaceGraphique/ExempleComboBox.java
@@ -0,0 +1,42 @@
+package interfaceGraphique;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ExempleComboBox {
+
+    public static void main(String[] args) {
+        String[] optionsToChoose = {"Artiste", "Titre", "Label", "Podcast", "Beatmaker", "Playlist"};
+
+        JFrame jFrame = new JFrame();
+
+        JComboBox<String> jComboBox = new JComboBox<>(optionsToChoose);
+        jComboBox.setBounds(100, 50, 100, 20);
+
+        JButton jButton = new JButton("Rechercher");
+        jButton.setBounds(100, 100, 110, 20);
+
+        JLabel jLabel = new JLabel();
+        jLabel.setBounds(90, 100, 400, 100);
+
+        jFrame.add(jButton);
+        jFrame.add(jComboBox);
+        jFrame.add(jLabel);
+
+        jFrame.setLayout(null);
+        jFrame.setSize(350, 250);
+        jFrame.setVisible(true);
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedFruit = "You selected " + jComboBox.getItemAt(jComboBox.getSelectedIndex());
+                jLabel.setText(selectedFruit);
+            }
+        });
+
+    }
+
+}
--- Music_Project_JAVA-main/Projet Musique - JAVA/src/interfaceGraphique/GUI.java
+++ Music_Project_JAVA-main/Projet Musique - JAVA/src/interfaceGraphique/GUI.java
@@ -26,40 +26,43 @@ public class GUI extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        panel = new JPanel();
+        JLabel jLabel = new JLabel();
+        jLabel.setBounds(90, 100, 400, 100);
+        panel.setLayout(null);
+        panel.add(jLabel);
+        panel.setLayout(null);
+        panel.setSize(350, 250);
+        panel.setVisible(true);
 
         nomArtiste = new PlaceholderTextField("");
         nomArtiste.setPlaceholder("Nom de l'artiste");
         nomArtiste.setFont( new Font("Comic sans ms",Font.BOLD,22));
         nomArtiste.setBounds(15, 100, 250, 40);
+        panel.add(nomArtiste);
 
         prenomArtiste = new PlaceholderTextField("");
         prenomArtiste.setPlaceholder("Prénom de l'artiste");
         prenomArtiste.setFont( new Font("Comic sans ms",Font.BOLD,22));
         prenomArtiste.setBounds(15, 150, 250, 40);
+        panel.add(prenomArtiste);
 
         pseudoArtiste = new PlaceholderTextField("");
         pseudoArtiste.setPlaceholder("Pseudo de l'artiste");
         pseudoArtiste.setFont( new Font("Comic sans ms",Font.BOLD,22));
         pseudoArtiste.setBounds(15, 200, 250, 40);
-
-
-        panel = new JPanel();
-        panel.setLayout(null);
-
-        panel.add(nomArtiste);
-        panel.add(prenomArtiste);
         panel.add(pseudoArtiste);
 
-        this.add(panel);
-
-        String[] optionsToChoose = {"Artiste", "Titre", "Label", "Podcast", "Beatmaker", "Playlist"};
-        mainComboBox= new JComboBox<>(optionsToChoose);
+        String[] optionsToChoose = { "Select Item", "Color", "Shape", "Fruit" };//{"Artiste", "Titre", "Label", "Podcast", "Beatmaker", "Playlist"};
+        mainComboBox= new JComboBox<String>(optionsToChoose);
         mainComboBox.addActionListener((ActionListener) this);
         mainComboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
         mainComboBox.setBounds(15, 40, 140, 20);
+        panel.add(mainComboBox);
 
         subComboBox = new JComboBox<String>();
         subComboBox.setPrototypeDisplayValue("XXXXXXXXXX");
+        panel.add(subComboBox);
 
         subItems = new Hashtable<String, String[]>();
         String[] subItems1 = { "Select Color", "Red", "Blue", "Green" };
@@ -69,12 +72,11 @@ public class GUI extends JFrame {
         String[] subItems3 = { "Select Fruit", "Apple", "Orange", "Banana" };
         subItems.put(optionsToChoose[3], subItems3);
 
-
         JButton jButton = new JButton("Rechercher");
         jButton.setBounds(500,140,110,25);
+        panel.add(jButton);
 
-        JLabel jLabel = new JLabel();
-        jLabel.setBounds(90, 100, 400, 100);
+        this.add(panel);
 
         jButton.addActionListener(new ActionListener() {
             @Override
@@ -83,7 +85,7 @@ public class GUI extends JFrame {
                 jLabel.setText(selectedCategory);
 
                 String item = (String)mainComboBox.getSelectedItem();
-                Object o = subItems.get( item );
+                Object o = subItems.get( optionsToChoose );
 
                 if (o == null)
                 {
@@ -95,13 +97,7 @@ public class GUI extends JFrame {
                 }
             }
         });
-        panel.add(jButton);
-        panel.add(mainComboBox);
-        panel.add(subComboBox);
-        panel.add(jLabel);
-        panel.setLayout(null);
-        panel.setSize(350, 250);
-        panel.setVisible(true);
+
     }
 
     public static void main(String[] args) {
